e303fd4c9c5efb9453cd3523e44f3fdfcfcf988f
Mathilde Mareite <mathilde.mareite@gmail.com>
2018-03-07 16:44:10 +0100

    Write articles in a file named corpus.txt along with the title and date

--- script.py
+++ script.py
@@ -7,21 +7,24 @@ Created on Mon Nov 13 16:55:37 2017
 
 from lxml import etree
 import numpy as np
+import os
 """
 import matplotlib.pyplot as plt // Comment this line for bug
 """
 
 total_article = 0
+file = open("corpus.txt","w") 
 
-years = range( 1971, 1990, 1)
+years = range( 1971, 1999, 1)
 month_list = ['01','02','03','04','05','06','07','08','09','10','11','12']
 journal_list = ['JDG','GDL']
 
-filecp = open( 'keywords_education.txt', encoding='utf-8' )
+filecp = open( 'keywords_international.txt', encoding='utf-8' )
 key_words = filecp.read().split('\n')
 
 for journal in journal_list:
     print("-------------------" + journal + "------------------------")
+    
     
     for year in years :
         print("-------------------" + str(year) + "------------------------")
@@ -29,45 +32,54 @@ for journal in journal_list:
         for month in month_list:
             print('-------Month : ' + month +'-------')
             
-            root = '../'+journal+'/'+ str(year) + '/' + month + '.xml'
-            tree = etree.parse(root)
             
-            articles = []
-            titles = []
-            dates=[]
+            root = '200yrs-news/'+journal+'/'+ str(year) + '/' + month + '.xml'
+            
+            if os.path.exists(root) :
+                tree = etree.parse(root)
 
-            for article in tree.xpath("/monthEntity/article/entity/full_text"):
-                articles.append(article.text)
+                articles = []
+                titles = []
+                dates =[]
 
-            for title in tree.xpath("/monthEntity/article/entity/meta/name"):
-                titles.append(title.text)
-                
-            for date in tree.xpath("/monthEntity/article/entity/meta/issue_date"):
-                dates.append(date.text)
-                
+                for article in tree.xpath("/monthEntity/article/entity/full_text"):
+                    articles.append(article.text)
 
-            for i in range(0,len(articles)):
-                
-                cpt = 0
-                kw = key_words.copy()
-                
-                try:
-                    words = articles[i].split(' ')
-                except(AttributeError):
-                    pass
+                for title in tree.xpath("/monthEntity/article/entity/meta/name"):
+                    titles.append(title.text)
 
-                
-                for j in range(0, len(words)):
+                for date in tree.xpath("/monthEntity/article/entity/meta/issue_date"):
+                    dates.append(date.text)
 
-                    
-                    words[j] = words[j].replace(',','')   #Remove the commas
-                    words[j] = words[j].replace('.', '')  #Remove the points
 
-                    if words[j] in kw  :
-                        kw.remove(words[j])
-                        cpt += 1
-                    
-                    if cpt >= 5 :
-                        total_article += 1
-                        print( total_article )
-                        break
+                for i in range(0,len(articles)):
+
+                    cpt = 0
+                    kw = key_words.copy()
+
+                    try:
+                        words = articles[i].split(' ')
+                    except(AttributeError):
+                        pass
+
+
+                    for j in range(0, len(words)):
+
+
+                        words[j] = words[j].replace(',','')   #Remove the commas
+                        words[j] = words[j].replace('.', '')  #Remove the points
+
+                        if words[j] in kw  :
+                            kw.remove(words[j])
+                            cpt += 1
+
+                        if cpt >= 8 :
+                            total_article += 1
+                            print( total_article )
+                            output = "**** *title_" + titles[i]
+                            output = output + " *date_" + dates[i]
+                            output = output + "\n" + articles[i] + "\n\n"
+                            file.write(output)
+                            break
+                            
+file.close()                 
